9cac0c36de02f2efec5d6fa86cad79888a539a92
Mohit030802 <mohitom2002@gmail.com>
2023-02-01 00:09:23 +0530

    Auth start

--- Server/index.js
+++ Server/index.js
@@ -2,6 +2,8 @@ import express from "express";
 import bodyParser from "body-parser";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
+import AuthRoute from './Routes/AuthRoute.js'
+import UserRoute from './Routes/UserRoute.js'
 
 const app = express();
 
@@ -13,3 +15,7 @@ mongoose
    process.env.MONGO_DB,{useNewUrlParser:true,useUnifiedTopology:true}
   )
   .then(() => app.listen(process.env.PORT, () => console.log(`Listening at ${process.env.PORT}`))).catch((error)=>console.log(error));
+
+
+  app.use('/auth',AuthRoute)
+  app.use('/user',UserRoute)
